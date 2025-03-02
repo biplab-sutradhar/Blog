385c808e4a015a1080a8b8c463156dc8630899df
Biplab Sutradhar <biplabsutradhar1453@gmail.com>
2025-03-02 23:47:25 +0530

    express add

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,21 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-app.post('/blogs', (req: Request, res: Response) => {});
-app.get('/blogs:id', (req: Request, res: Response) => {});
-app.get('/blogs', (req: Request, res: Response) => {});
-app.patch('/blogs:id', (req: Request, res: Response) => {});
-app.delete('/blogs:id', (req: Request, res: Response) => {});
+app.post('/blogs', (req: Request, res: Response) => {
+  res.send('Create a new blog');
+});
+app.get('/blogs:id', (req: Request, res: Response) => {
+  res.send('Get a blog by ID');
+});
+app.get('/blogs', (req: Request, res: Response) => {
+  res.send('Get all blogs');
+});
+app.patch('/blogs:id', (req: Request, res: Response) => {
+  res.send('Update a blog by ID');
+});
+app.delete('/blogs:id', (req: Request, res: Response) => {
+  res.send('Delete a blog by ID');
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
